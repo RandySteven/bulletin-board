70b091974a6e83b0102b7b2a8d0b2b4421bc05dc
Randy Steven <randysteven12@gmail.com>
2024-06-02 23:15:53 +0700

    binding method

--- utils/rest.go
+++ utils/rest.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/iancoleman/strcase"
+	"io"
 	"log"
 	"net/http"
 	"reflect"
@@ -19,31 +20,74 @@ func BindJSON(req *http.Request, request interface{}) error {
 	return json.NewDecoder(req.Body).Decode(&request)
 }
 
-func BindForm(req *http.Request, request any) error {
+func BindForm(req *http.Request, request interface{}) error {
 	err := req.ParseForm()
 	if err != nil {
 		return err
 	}
-	bindForm := make(map[string]interface{})
+
+	requestValue := reflect.ValueOf(request)
+	requestElem := requestValue.Elem()
+
+	formValues := make(map[string]reflect.Value)
+
 	for key, values := range req.Form {
-		log.Printf("key : %s value : %s", key, values[0])
-		bindForm[key] = values[0]
+		snakeKey := strcase.ToSnake(key)
+		field := requestElem.FieldByName(snakeKey)
+		if !field.IsValid() {
+			log.Printf("Field not found in request: %s", snakeKey)
+			continue
+		}
+		if !field.CanSet() {
+			return fmt.Errorf("Field %s is not settable", snakeKey)
+		}
+		formValue := reflect.ValueOf(values[0])
+		if !formValue.Type().ConvertibleTo(field.Type()) {
+			return fmt.Errorf("Type conversion error for field %s", snakeKey)
+		}
+		convertedValue := formValue.Convert(field.Type())
+		field.Set(convertedValue)
+		formValues[snakeKey] = convertedValue
 	}
 
-	fields := GetFieldsOfObject(request)
+	return nil
+}
 
-	for _, field := range fields {
-		val := reflect.ValueOf(bindForm[strcase.ToSnake(field)])
-		if val.IsValid() {
-			fieldValue := reflect.ValueOf(request).Elem().FieldByName(field)
-			if fieldValue.IsValid() && fieldValue.CanSet() {
-				if val.Type().AssignableTo(fieldValue.Type()) {
-					fieldValue.Set(val)
-				} else {
-					return fmt.Errorf("type conversion error for field %s", field)
-				}
-			}
+func BindMultipartForm(req *http.Request, request interface{}) error {
+	err := req.ParseMultipartForm(32 << 20) // Adjust max memory size as needed
+	if err != nil {
+		return err
+	}
+	err = BindForm(req, request)
+	if err != nil {
+		return err
+	}
+	multipartFiles := req.MultipartForm.File
+
+	for fieldname, files := range multipartFiles {
+		field := reflect.ValueOf(request).Elem().FieldByName(fieldname)
+		if !field.IsValid() {
+			log.Printf("Field not found in request for file upload: %s", fieldname)
+			continue
 		}
+
+		if field.Kind() != reflect.Slice || !reflect.TypeOf(field.Interface()).Elem().Implements(reflect.TypeOf((*io.Reader)(nil)).Elem()) {
+			return fmt.Errorf("Field %s is not a slice of io.Reader", fieldname)
+		}
+
+		fieldSlice := reflect.MakeSlice(field.Type(), 0, len(files))
+
+		for _, fileHeader := range files {
+			uploadedFile, err := fileHeader.Open()
+			if err != nil {
+				return fmt.Errorf("Error opening uploaded file %s: %w", fileHeader.Filename, err)
+			}
+			defer uploadedFile.Close() // Close the file after processing
+
+			fieldSlice = reflect.Append(fieldSlice, reflect.ValueOf(uploadedFile))
+		}
+
+		field.Set(fieldSlice)
 	}
 
 	return nil
