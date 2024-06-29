0f0a73f684557d8eae8327c7b79107972f433e75
Randy Steven <randysteven12@gmail.com>
2024-06-30 00:43:14 +0700

    add struct

--- entities/dtos/responses/chat_response.go
+++ entities/dtos/responses/chat_response.go
@@ -10,10 +10,24 @@ type (
 		CreatedAt time.Time             `json:"created_at"`
 	}
 
+	RoomDetailResponse struct {
+		ID    uint64               `json:"id"`
+		Chats []ChatDetailResponse `json:"chats"`
+	}
+
 	RoomListResponse struct {
 		ID    uint64                `json:"id"`
 		User1 *UserRoomChatResponse `json:"user1"`
 		User2 *UserRoomChatResponse `json:"user2"`
+	}
+
+	ChatDetailResponse struct {
+		ID   uint64 `json:"id"`
+		User struct {
+			ID       uint64 `json:"id"`
+			UserName string `json:"user_name"`
+		} `json:"user"`
+		Message string `json:"message"`
 	}
 
 	ChatResponse struct {
