0d1728e5584d7dcc12140a37d92fc0341dfc7eb0
Randy Steven <randysteven12@gmail.com>
2024-07-06 13:57:47 +0700

    refactoring

--- entities/dtos/requests/user_request.go
+++ entities/dtos/requests/user_request.go
@@ -2,17 +2,19 @@ package requests
 
 import "task_mission/enums"
 
-type UserRegisterRequest struct {
-	FirstName   string           `json:"first_name" validate:"required"`
-	LastName    string           `json:"last_name" validate:"required"`
-	UserName    string           `json:"user_name" validate:"required"`
-	DateOfBirth string           `json:"date_of_birth" validate:"date_of_birth"`
-	Gender      enums.UserGender `json:"gender" validate:"required"`
-	Email       string           `json:"email" validate:"email"`
-	Password    string           `json:"password" validate:"password"`
-}
+type (
+	UserRegisterRequest struct {
+		FirstName   string           `json:"first_name" validate:"required"`
+		LastName    string           `json:"last_name" validate:"required"`
+		UserName    string           `json:"user_name" validate:"required"`
+		DateOfBirth string           `json:"date_of_birth" validate:"date_of_birth"`
+		Gender      enums.UserGender `json:"gender" validate:"required"`
+		Email       string           `json:"email" validate:"email"`
+		Password    string           `json:"password" validate:"password"`
+	}
 
-type UserLoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
+	UserLoginRequest struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+)
