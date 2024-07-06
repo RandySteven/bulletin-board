c60bca9ce08eb7ee7afe3c2f6749bc210d977580
Randy Steven <randysteven12@gmail.com>
2024-07-06 13:12:58 +0700

    implements cors properly

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -1,11 +1,13 @@
 package middlewares
 
 import (
+	"log"
 	"net/http"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("CORS")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
