7371495340ab3af1a53a82415471113a11f23c5c
Randy Steven <randysteven12@gmail.com>
2024-07-14 23:23:38 +0700

    fix cors middleware

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -11,7 +11,8 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		w.Header().Set("Access-Control-Request-Method", "POST, OPTIONS, GET, PUT, DELETE")
+		w.Header().Set("Access-Control-Request-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		w.Header().Set("Content-Type", "application/json")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
